0074cf38ad86dbbfd6e069c09b42b709922a6d33
Alexobai <shhuang@clarkson.edu>
2019-10-29 17:55:20 -0400

    Buttons and onclick

---
+++ app/src/main/java/com/example/helloworld/ButtonActivity.java
@@ -0,0 +1,37 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ButtonActivity extends AppCompatActivity {
+
+    private Button mBtn3;
+    private TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+        mBtn3 = findViewById(R.id.btn_3);
+        mBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ButtonActivity.this,"button 3 is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mTextView = findViewById(R.id.tv_1);
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ButtonActivity.this,"text view is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void showToast(View view){
+        Toast.makeText(this,"Test for onclick button 4", Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private Button mbtnTextView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //jump to textview display
                 Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        mButton = findViewById(R.id.btn_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ButtonActivity.class);
                 startActivity(intent);
             }
         });
