48f9ef01e06ee03109d65ca949de45d5ad1c3562
Alexobai <shhuang@clarkson.edu>
2019-11-05 16:08:47 -0500

    Animation

--- app/src/main/java/com/example/helloworld/ObjectAnimActivity.java
+++ app/src/main/java/com/example/helloworld/ObjectAnimActivity.java
@@ -2,7 +2,10 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ObjectAnimActivity extends AppCompatActivity {
@@ -13,6 +16,25 @@ public class ObjectAnimActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_object_anim);
         mTextView = findViewById(R.id.tv);
-        mTextView.animate().rotationYBy(500).setDuration(2000).start();
+        //mTextView.animate().translationYBy(500).setDuration(2000).start();
+        //mTextView.animate().translationY(500).setDuration(2000).start();
+        //mTextView.animate().alpha(0).setDuration(2000).start();
+
+//        ValueAnimator valueAnimator = ValueAnimator.ofInt(0,100);
+//        valueAnimator.setDuration(2000);
+//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator animation) {
+//                //actual number
+//                Log.d("anime",animation.getAnimatedValue()+"");
+//                //process number 0-1
+//                Log.d("anime",animation.getAnimatedFraction()+"");
+//            }
+//        });
+//        valueAnimator.start();
+
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(mTextView,"translationY",0,500,200,300);
+        objectAnimator.setDuration(2000);
+        objectAnimator.start();
     }
 }
